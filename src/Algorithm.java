ec482e199564b1529ab862283879816ad41334fd
Nikanorenkov-Mihail <mihanik03@gmail.com>
2024-05-25 04:01:38 +0300

    Final with queue

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+
+import static java.lang.Math.abs;
 
 public class Algorithm {      // Класс различных функций
     //Parameters
@@ -14,12 +14,13 @@ public class Algorithm {      // Класс различных функций
     double T;                    // текущее системное время
     double T_post;               // ближайший момент времени поступления заявки в систему
     double[] T_k;     // суммарные времена занятости каналов (массив, размерность K)
-
+    double[] time_queue;       // хранение информации о времени поступления заявки
     //Enter parameters
     public int N_max;            // число реализаций – максимальное число заявок, поступивших в систему (вводимый параметр)
     public static int K;         // число каналов (вводимый параметр)
     public int IntensiveIn;      // интенсивность поступления заявок (вводимый параметр)
     public double IntensiveService; // интенсивность обслуживания (вводимый параметр)
+    public int t_2;              // максимальное время нахождения заявки в очереди (параметр выхода заявки из очереди)
 
     //Generated_values
     public double t_osv;            // длительность обслуживания – длительность интервала между моментами начала обслуживания и освобождения прибора (случайная величина)
@@ -46,13 +47,18 @@ public class Algorithm {      // Класс различных функций
                             break;
                         case ("IntensiveService"):
                             IntensiveService = Double.parseDouble(words[1]);
-                            //IntensiveService = Integer.parseInt(words[1]);
                             break;
                         case ("N_max"):
                             N_max = Integer.parseInt(words[1]);
                             break;
                         case ("K"):
                             K = Integer.parseInt(words[1]);
+                            break;
+                        case ("t_2"):
+                            t_2 = Integer.parseInt(words[1]);
+                            break;
+                        default:
+                            System.out.println("Ошибка во входном файле");
                             break;
                     }
             }
@@ -70,8 +76,40 @@ public class Algorithm {      // Класс различных функций
         this.T_post = 0; // ближайший момент времени поступления заявки в систему
         T_k = new double[K];
         T_osv = new double[K];
+        time_queue = new double[N_max];
         Arrays.fill(this.T_k, 0);    // суммарные времена занятости каналов (массив, размерность K)
         Arrays.fill(this.T_osv, 99999999);  // ближайшие моменты времени освобождения приборов (массив, размерность K)
+        Arrays.fill(this.time_queue, 0);
+    }
+
+    public void timeCheck() {
+        for (int i = 0; i < this.time_queue.length; i++) {
+            if (abs(this.T - this.time_queue[i]) > this.t_2 & this.time_queue[i] != 0.0)
+                deleteTime(i);
+        }
+    }
+
+    public void deleteTime(int number) {
+        if (number == this.time_queue.length - 1) this.time_queue[number] = 0;
+        else {
+            for (int i = number; i < this.time_queue.length - 1; i++) {
+                this.time_queue[i] = this.time_queue[i + 1];
+            }
+            System.out.println("Заявка номер(-):" + (number + 1) + " удалена из очереди (превышено время ожидания в очереди) | Текущее время: " + this.T + " | Заявка №2 становится заявкой №1");
+        }
+    }
+
+    public void addTime() {
+        boolean flagToOver = false;
+        for (int i = 0; i < this.time_queue.length - 1; i++) {
+            if (this.time_queue[i] == 0.0 & !flagToOver) {
+                this.time_queue[i] = this.T;
+                flagToOver = true;
+                System.out.println("Заявка номер(+): " + (i + 1) + " | Поступила в: " + this.T);
+                i = this.time_queue.length - 1;
+            }
+        }
+        if (!flagToOver) System.out.println("Слишком много заявок поступает в очередь");
     }
 
     public double Form_rand() {
@@ -94,7 +132,7 @@ public class Algorithm {      // Класс различных функций
     public double Form_K_z() {
         double sum = 0;
         for (int i = 0; i < K; i++) {
-            sum += (i + 1) * ((double) this.T_k[i] / this.T);
+            sum += (i + 1) * (this.T_k[i] / this.T);
         }
         return sum;
     }
@@ -110,7 +148,7 @@ public class Algorithm {      // Класс различных функций
     public boolean StoppingCondition() {
         return (this.N_post >= this.N_max);
     }
-
+/*
     public void show() {
         System.out.println("T_pred " + this.T_pred);
         System.out.println("T " + this.T);
@@ -121,6 +159,16 @@ public class Algorithm {      // Класс различных функций
         System.out.println("t_post " + t_post);
         System.out.println("__________");
     }
+
+ */
+
+    public void showResults() {
+        System.out.println("__________Results__________");
+        System.out.println("Result Ppr: " + this.P_pr);
+        System.out.println("Result Kz: " + this.K_z);
+        System.out.println("__________Results__________");
+    }
+
 
     public Algorithm(String inputFile) {
         //enter_parameters("input_var_14.txt");
@@ -137,7 +185,9 @@ public class Algorithm {      // Класс различных функций
 
             if (this.t_post < minOf_T_osv()) {
                 this.T = this.T_post;
+                timeCheck();
                 this.N++;
+                addTime();
                 //show();
                 this.T_k[N_k] += this.T - this.T_pred;
 
@@ -152,27 +202,31 @@ public class Algorithm {      // Класс различных функций
                 T_post += t_post;
                 N_post++;
                 //show();
+
                 if (StoppingCondition()) {
-                    this.P_pr = (double) this.T_k[0] / this.T;
-
+                    this.P_pr = this.T_k[0] / this.T;
                     this.K_z = Form_K_z();
-
-                    System.out.println("Result Ppr: " + this.P_pr);
-                    System.out.println("Result Kz: " + this.K_z);
+                    showResults();
                 }
 
             } else {
                 this.T = minOf_T_osv();
-                this.N--;
+                timeCheck();
+                if (this.N > 0) {
+                    this.N--;
+                    this.T_k[this.N_k] += this.T - this.T_pred;
 
-                this.T_k[this.N_k] += this.T - this.T_pred;
+                    //Arrays.fill(this.T_osv, this.T);   Условие не нужно, так как T_osv позже целиком перезаполнится
+                    if (this.N < K) {
+                        Arrays.fill(this.T_osv, 99999999);
+                        this.N_k--;
+                    } else {
+                        Arrays.fill(this.T_osv, this.T + this.t_osv);
+                    }
 
-                //Arrays.fill(this.T_osv, this.T);   Условие не нужно, так как T_osv позже целиком перезаполнится
-                if (this.N < K) {
-                    Arrays.fill(this.T_osv, 99999999);
-                    this.N_k--;
                 } else {
-                    Arrays.fill(this.T_osv, this.T + this.t_osv);
+                    System.out.println("Отсутствуют заявки в системе");
+                    System.out.println("Время: " + this.T);
                 }
             }
         }
